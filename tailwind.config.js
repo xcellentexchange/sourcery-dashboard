d7cb8ad95e65cf7bb8a2d518aceb216db57716d8
Jason Sims <jason@jasonsims.dev>
2025-12-04 15:10:18 -0500

    Refactored tailwind.config.js to use named imports for plugins and improved formatting for better readability.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,26 @@
+import formsPlugin from "@tailwindcss/forms";
+import typographyPlugin from "@tailwindcss/typography";
+
 export default {
-  darkMode: 'class',
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      keyframes: {
-        progress: {
-          '0%': { width: '0%' },
-          '80%': { opacity: 1 },
-          '100%': { opacity: 0.5, width: '100%' },
-        },
-      },
-      animation: {
-        progress: 'progress 1 forwards linear',
-      },
-      transitionProperty: {
-        width: 'width',
-        spacing: 'margin, padding',
-      },
-    },
-  },
-  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
-}
+	darkMode: "class",
+	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			keyframes: {
+				progress: {
+					"0%": { width: "0%" },
+					"80%": { opacity: 1 },
+					"100%": { opacity: 0.5, width: "100%" },
+				},
+			},
+			animation: {
+				progress: "progress 1 forwards linear",
+			},
+			transitionProperty: {
+				width: "width",
+				spacing: "margin, padding",
+			},
+		},
+	},
+	plugins: [formsPlugin, typographyPlugin],
+};
